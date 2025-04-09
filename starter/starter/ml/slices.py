7959ff6975c2b87f42aa43702f4c64f0e17a9622
Raghul Srinivas <RSrinivas@evolent.com>
2025-04-09 04:45:55 -0400

    Adding slices pipelines.

---
+++ starter/starter/ml/slices.py
@@ -0,0 +1,125 @@
+import pandas as pd
+
+
+def data_slices(data: pd.DataFrame, label: str) -> dict:
+    """
+    Function to create slices of the data for each categorical feature.
+
+    Inputs
+    ------
+    data : pd.DataFrame
+        Dataframe containing the features and label.
+    label : str
+        Name of the label column in `data`.
+
+    Returns
+    -------
+    slices : dict
+        Dictionary containing the slices of the data for each categorical feature.
+    """
+
+    slices = {}
+
+    # Define age buckets
+    age_bins = [0, 18, 29, 44, 59, 100]
+    age_labels = ["0-18", "19-29", "30-44", "45-59", "60+"]
+    data["age_data_slice"] = pd.cut(data["age"], bins=age_bins, labels=age_labels, right=True)
+    slices["age_data_slice"] = {label: data[data["age_data_slice"] == label] for label in age_labels}
+
+    # Define marital status buckets
+    marital_status_mapping = {
+        " Married-civ-spouse": "Married",
+        " Married-spouse-absent": "Married",
+        " Married-AF-spouse": "Married",
+        " Divorced": "Divorced",
+        " Never-married": "Never-married",
+        " Separated": "Separated",
+        " Widowed": "Widowed",
+    }
+    data["marital_status_data_slices"] = data["marital-status"].map(
+        lambda x: marital_status_mapping[x].replace(" ", "")
+    )
+    slices["marital_status_data_slice"] = {
+        label: data[data["marital_status_data_slices"] == label] for label in marital_status_mapping.values()
+    }
+
+    # Define Race buckets
+    data["race_data_slices"] = pd.Categorical(data["race"], categories=data["race"].unique())
+    slices["race_data_slice"] = {label: data[data["race_data_slices"] == label] for label in data["race"].unique()}
+
+    # Define Sex buckets
+    data["sex_data_slices"] = pd.Categorical(data["sex"], categories=data["sex"].unique())
+    slices["sex_data_slice"] = {label: data[data["sex_data_slices"] == label] for label in data["sex"].unique()}
+
+    # Define combined buckets for occupation and workclass
+    # data["occupation_workclass_bucket"] = data["occupation"] + "_" + data["workclass"]
+    # data["occupation_workclass_data_slices"] = pd.Categorical(
+    #     data["occupation_workclass_bucket"], categories=data["occupation_workclass_bucket"].unique()
+    # )
+    # slices["occupation_workclass_data_slice"] = {
+    #     label: data[data["occupation_workclass_data_slices"] == label]
+    #     for label in data["occupation_workclass_bucket"].unique()
+    # }
+
+    # Define native country buckets
+    country_mapping = {
+        "United-States": "North America",
+        "Canada": "North America",
+        "Mexico": "North America",
+        "Puerto-Rico": "North America",
+        "Outlying-US(Guam-USVI-etc)": "North America",
+        "Cuba": "Central and South America",
+        "Jamaica": "Central and South America",
+        "Dominican-Republic": "Central and South America",
+        "El-Salvador": "Central and South America",
+        "Guatemala": "Central and South America",
+        "Honduras": "Central and South America",
+        "Nicaragua": "Central and South America",
+        "Peru": "Central and South America",
+        "Ecuador": "Central and South America",
+        "Columbia": "Central and South America",
+        "Trinadad&Tobago": "Central and South America",
+        "England": "Europe",
+        "Germany": "Europe",
+        "Greece": "Europe",
+        "Italy": "Europe",
+        "Poland": "Europe",
+        "Portugal": "Europe",
+        "Ireland": "Europe",
+        "France": "Europe",
+        "Scotland": "Europe",
+        "Yugoslavia": "Europe",
+        "Holand-Netherlands": "Europe",
+        "Cambodia": "Asia",
+        "India": "Asia",
+        "Japan": "Asia",
+        "China": "Asia",
+        "Iran": "Asia",
+        "Philippines": "Asia",
+        "Vietnam": "Asia",
+        "Laos": "Asia",
+        "Taiwan": "Asia",
+        "Thailand": "Asia",
+        "Hong": "Asia",
+        "South": "Other",
+        "Haiti": "Other",
+        "Hungary": "Other",
+    }
+
+    country_mapping_space = country_mapping
+    country_mapping_space = {f" {k}": v for k, v in country_mapping_space.items()}
+
+    data["country_data_slices"] = data["native-country"].map(
+        lambda x: country_mapping_space[x] if x in country_mapping_space else "Other"
+    )
+    slices["country_data_slice"] = {
+        label: data[data["country_data_slices"] == label] for label in country_mapping_space.values()
+    }
+
+    # Combine age buckets and education into a new column
+    # data["age_education_data_slices"] = data["age_data_slice"].astype(str) + "_" + data["education"]
+    # slices["age_education_data_slice"] = {
+    #     label: data[data["age_education_data_slices"] == label] for label in data["age_education_data_slices"].unique()
+    # }
+
+    return slices
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -4,18 +4,22 @@ from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 from ml.data import process_data
+from ml.slices import data_slices
 import os
 import joblib
 from ml.model import train_model, inference, compute_model_metrics
-import pandas
+import pandas as pd
 from datetime import datetime
+
+
+test_processed_dataset = None
 
 
 def load_data():
     """
     Load the data from the CSV file.
     """
-    data = pandas.read_csv(
+    data = pd.read_csv(
         "/data/home/corp.evolenthealth.com/rsrinivas/Udacity/MLOps/course_4_project/starter/data/census_udacity.csv"
     )
     data.columns = [col.strip() for col in data.columns]
@@ -29,7 +33,7 @@ def training_pipeline():
     print("Data loaded successfully.", datetime.now())
 
     # Optional enhancement, use K-fold cross validation instead of a train-test split.
-    train, test = train_test_split(data, test_size=0.20)
+    train, test = train_test_split(data, test_size=0.20, random_state=42)
 
     cat_features = [
         "workclass",
@@ -57,6 +61,11 @@ def training_pipeline():
     joblib.dump(lb, os.path.join(base_path, "label_binarizer.pkl"))
 
     pred = inference(model, X_test)
+    copy_df = pd.DataFrame(test).copy()
+    copy_df["pred"] = pd.Series(pred, index=copy_df.index)
+    copy_df["salary_binary"] = copy_df["salary"].apply(lambda x: 1 if x == ">50K" else 0)
+    global test_processed_dataset
+    test_processed_dataset = copy_df
 
     precision, recall, fbeta = compute_model_metrics(y_test, pred)
     print(f"Precision: {precision}")
@@ -64,6 +73,32 @@ def training_pipeline():
     print(f"Fbeta: {fbeta}")
 
 
+def slices_pipeline():
+    """
+    Function to create slices of the data for each categorical feature.
+    """
+    slices = data_slices(test_processed_dataset, "salary")
+    # Compute metrics for each slice
+    for slice_name, slice_data in slices.items():
+        print(f"Metrics for slice: {slice_name}")
+        for slice_key, slice_value in slice_data.items():
+            print(f"\tSlice Bucket: {slice_key}")
+            # Process the slice data
+            y_slice = slice_value["salary_binary"]
+            pred_slice = slice_value["pred"]
+            # Compute metrics for the slice
+            precision, recall, fbeta = compute_model_metrics(y_slice, pred_slice)
+            print(f"\t\tTotal: {slice_value.shape[0]}\n")
+            print(f"\t\t<=50K counts: {slice_value[slice_value['salary_binary'] == 0].shape[0]}")
+            print(f"\t\t>50K counts: {slice_value[slice_value['salary_binary'] == 1].shape[0]}\n")
+            print(f"\t\tPrecision: {precision}")
+            print(f"\t\tRecall: {recall}")
+            print(f"\t\tFbeta: {fbeta}")
+            print()
+
+
 if __name__ == "__main__":
     training_pipeline()
     print("Training Script finished.", datetime.now())
+    slices_pipeline()
+    print("Slices Script finished.", datetime.now())
